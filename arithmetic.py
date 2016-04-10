42e94b75a78cd426d49eeba00df486eb611877d9
cvlong <cvlong@gmail.com>
2016-04-10 11:54:43 -0700

    added docstrings

--- arithmetic.py
+++ arithmetic.py
@@ -1,36 +1,50 @@
 def add(num1, num2):
+    """return the sum of two input integers"""
+
     return num1 + num2
 
 
 def subtract(num1, num2):
+    """return the second input subtracted from the first input"""
+
     return num1 - num2
 
 
 def multiply(num1, num2):
+    """multiply two inputs together"""
+
     return num1 * num2
 
 
 def divide(num1, num2):
+    """divide the first input by the second input; return a float"""
     # Need to turn at least argument to float for division to
     # not be integer division
+    
     return float(num1) / float(num2) 
 
 
 def square(num1):
-    # Needs only one argument
-    return num1 * num1
+    """return the square of the input"""
+    
+    return num1 ** 2
 
 
 def cube(num1):
-    # Needs only one argument
-    return num1 * num1 * num1
+    """return the cube of the input"""
+    
+    return num1 ** 3
 
 
 def power(num1, num2):
-    return num1 ** num2  # ** = exponent operator
+    """raise the first input by the second input"""
+
+    return num1 ** num2
 
 
 def mod(num1, num2):
+    """return the remainder when the first input is divided by the second input""" 
+    
     return num1 % num2
 
 
